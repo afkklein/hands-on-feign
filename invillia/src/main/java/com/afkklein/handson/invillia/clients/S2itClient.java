98ab6833b4bdc90aaa42174110cc3e06750e97d0
Augusto Klein <afkklein@gmail.com>
2020-09-25 07:23:12 -0400

    Utilizando o ribbon para fazer o client side load balance

--- invillia/src/main/java/com/afkklein/handson/invillia/clients/S2itClient.java
+++ invillia/src/main/java/com/afkklein/handson/invillia/clients/S2itClient.java
@@ -1,10 +1,12 @@
 package com.afkklein.handson.invillia.clients;
 
 import com.afkklein.handson.invillia.dto.MessageS2itDTO;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @FeignClient(name="s2it", url="localhost:8000")
+@RibbonClient("s2it")
 public interface S2itClient {
 
     @GetMapping("/messages")
