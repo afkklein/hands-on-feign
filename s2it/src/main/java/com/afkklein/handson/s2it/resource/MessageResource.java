7e545dabe0b6625df55a72b80f14083cb5a39469
Augusto Klein <afkklein@gmail.com>
2020-09-26 08:40:40 -0400

    Utilização do hystrix para criar uma resiliência com fallback em caso de ocorrer exceções

--- s2it/src/main/java/com/afkklein/handson/s2it/resource/MessageResource.java
+++ s2it/src/main/java/com/afkklein/handson/s2it/resource/MessageResource.java
@@ -2,6 +2,7 @@ package com.afkklein.handson.s2it.resource;
 
 import com.afkklein.handson.s2it.dto.MessageDTO;
 import com.afkklein.handson.s2it.service.MessageService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +13,14 @@ public class MessageResource {
     @Autowired
     MessageService messageService;
 
+    @HystrixCommand(fallbackMethod = "fallback")
     @GetMapping("/messages")
     public MessageDTO getMessage() {
+        throw new RuntimeException("Some error");
+//        return messageService.getMessage();
+    }
+
+    public MessageDTO fallback() {
         return messageService.getMessage();
     }
 }
