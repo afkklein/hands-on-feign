2b3cbc2ddd166d3b6a9f362af59498714bd8d6d6
Augusto Klein <afkklein@gmail.com>
2020-10-24 09:58:14 -0400

    Correçao de code smell relacionado ao tipo de retorno da mensagem

--- s2it/src/main/java/com/afkklein/handson/s2it/resource/MessageResource.java
+++ s2it/src/main/java/com/afkklein/handson/s2it/resource/MessageResource.java
@@ -1,12 +1,15 @@
 package com.afkklein.handson.s2it.resource;
 
 import com.afkklein.handson.s2it.dto.ResponseMessageDto;
+import com.afkklein.handson.s2it.model.Autor;
 import com.afkklein.handson.s2it.repository.AutorRepository;
 import com.afkklein.handson.s2it.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/autores")
@@ -19,7 +22,7 @@ public class MessageResource {
     AutorRepository autorRepository;
 
     @GetMapping
-    public ResponseMessageDto<?> listaAutores() {
+    public ResponseMessageDto<List<Autor>> listaAutores() {
         return new ResponseMessageDto<>(autorRepository.findAll(), messageService.retornaPorta());
     }
 
